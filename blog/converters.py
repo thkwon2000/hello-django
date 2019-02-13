3b089d54587672d0ed16ca25e2ed5550775060b7
thkwon2000 <44591883+thkwon2000@users.noreply.github.com>
2019-02-13 12:16:02 +0900

    shop 앱 생성 및 등록

---
+++ blog/converters.py
@@ -0,0 +1,14 @@
+from django.urls.converters import StringConverter
+
+class SlugUnicodeConverter (StringConverter):
+    regex = r"[-\w]+"
+
+
+class FourDigitYearConverter:
+    regex = r'2[01]\d{2}'
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return '%04d' % value

c9b7d1e4a479d69799f45ddbdcba7322b04c6ddb
thkwon2000 <44591883+thkwon2000@users.noreply.github.com>
2019-02-13 12:14:16 +0900

    shop 앱 생성 및 등록

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -14,19 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import index, hello_times
-from blog.views import articles_by_year
+from django.urls import include, path
+#from blog.views import index, hello_times
+#from blog.views import articles_by_year
 
-from django.urls import register_converter
-from blog.converters import FourDigitYearConverter
+#from django.urls import register_converter
+#from blog.converters import FourDigitYearConverter
+#from django.urls.coverters import StringConverter
 
-register_converter(FourDigitYearConverter, 'year')
+
+#register_converter(FourDigitYearConverter, 'year')
 
 urlpatterns = [
-    path('articles/<year:year>/', articles_by_year),
+ #   path('articles/<year:year>/', articles_by_year),
     path('admin/', admin.site.urls),
-    path('blog/hello_times/<int:times>/',hello_times),
+    path('blog/', include('blog.urls')),
+  #  path('blog/hello_times/<int:times>/',hello_times),
     # re_path(r'blog/hello_times/(?P<times>\d+)/$',hello_times), 위의것과 동일함(정규표현식임)
-    path('', index),
+  #  path('', index),
+    path('shop/', include('shop.urls')),
 ]
